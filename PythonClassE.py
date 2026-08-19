4d043c49866c79f305aad73a86cc7238c20d458e
Biancorine <Lybra396@gmail.com>
2026-08-19 20:50:09 +0000

    Added Problem E

---
+++ PythonClassE.py
@@ -0,0 +1,14 @@
+print("\n", end="")
+
+
+Valor = int(input("Valor (Reais): "))
+print("\n", end="")
+Taxa = int(input("Taxa: "))
+Tempo = int(input("Tempo: "))
+
+
+print("\n", end="")
+print("Distância Percorrida: " + str(Distancia))
+print("\n", end="")
+print("Litros de gasolina usados: " + str(Litros_Usados))
+print("\n", end="")
