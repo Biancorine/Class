1328cb1537cbedfd122b335536b9a5f7f0f1d0df
Biancorine <Lybra396@gmail.com>
2026-08-19 19:49:57 +0000

    Program Complete

---
+++ PythonClassA.py
@@ -0,0 +1,9 @@
+
+print("\n", end="")
+
+Celsius = int(input("Temperatura em Celsius:"))
+
+Fahrenheit = (9 * Celsius + 160) / 5
+
+print("\n", end="")
+print("Temperatura em Fahrenheit: " + str(Fahrenheit))
