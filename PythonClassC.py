897c46061145ea1158263c570919fae530d764d6
Biancorine <Lybra396@gmail.com>
2026-08-19 20:11:09 +0000

    Second class assignment complete

---
+++ PythonClassC.py
@@ -0,0 +1,13 @@
+print("\n", end="")
+
+PI = 3.14
+
+Raio = int(input("Raio: "))
+print("\n", end="")
+Altura = int(input("Altura: "))
+
+Volume = PI * Raio**2 * Altura
+
+print("\n", end="")
+print("Volume da lata: " + str(Volume))
+print("\n", end="")
