310d75e6318ffc7865f8059eb100b62d9b4e688a
Biancorine <Lybra396@gmail.com>
2026-08-20 09:53:46 -0300

    Added Problem H

---
+++ PythonClassH.py
@@ -0,0 +1,13 @@
+print("\n", end="")
+
+Comprimento = int(input("Comprimento: "))
+print("\n", end="")
+Largura= int(input("Largura: "))
+print("\n", end="")
+Altura = int(input("Altura: "))
+
+Volume = Comprimento * Largura * Altura
+
+print("\n", end="")
+print("Volume do Retângulo 3D: " + str(Volume))
+print("\n", end="")
