11ab269731040880d24e0aa39a1815f97d72a536
Biancorine <Lybra396@gmail.com>
2026-08-26 07:45:37 -0300

    New Lesson

---
+++ Problem 02/PythonA.py
@@ -0,0 +1,10 @@
+
+print("\n", end="")
+
+Base = int(input("Base do Triângulo:"))
+Altura = int(input("Altura do Triângulo:"))
+
+Area = Base * Altura / 2
+
+print("\n", end="")
+print("Area do Triangulo: " + str(Area))
