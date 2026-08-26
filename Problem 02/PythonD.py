fd166e271d340bb8e44c85d83fe63bc093a12654
Biancorine <Lybra396@gmail.com>
2026-08-26 08:14:27 -0300

    problemD complete

---
+++ Problem 02/PythonD.py
@@ -0,0 +1,15 @@
+print("\n", end="")
+
+PI = 3.14
+
+Perimetro = int(input("Perimetro: "))
+print("\n", end="")
+Diametro= Perimetro / PI
+print("\n", end="")
+Raio = Diametro / 2
+
+Area = Raio ** PI
+
+print("\n", end="")
+print("Area: " + str(Area))
+print("\n", end="")
