0959aaca05d340abb0c48cd4ba4959f0de9bd5fa
Biancorine <Lybra396@gmail.com>
2026-09-06 14:06:10 -0300

    halfway

---
+++ Problem 03/Assignment_B_e.py
@@ -0,0 +1,9 @@
+import math
+
+A = float(input("Digite o valor de A: "))
+B = float(input("Digite o valor de B: "))
+C = float(input("Digite o valor de C: "))
+
+Delta = B**2 - 4*A*C
+
+print(Delta)
