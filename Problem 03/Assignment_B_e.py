53969d26871ffab57b7cb0dffc17a4b0935ba76f
Biancorine <Lybra396@gmail.com>
2026-09-06 17:13:09 -0300

    Assingments COmplete

--- Problem 03/Assignment_B_e.py
+++ Problem 03/Assignment_B_e.py
@@ -6,4 +6,17 @@ C = float(input("Digite o valor de C: "))
 
 Delta = B**2 - 4*A*C
 
-print(Delta)
+if Delta > 0:
+    Raiz1 = (-B + math.sqrt(Delta)) / (2*A)
+    Raiz2 = (-B - math.sqrt(Delta)) / (2*A)
+
+    print(f"X1 = {Raiz1}")
+    print(f"X2 = {Raiz2}")
+
+elif Delta == 0:
+    Raiz1 = -B / (2*A)
+
+    print(f"X1 e X2 = {Raiz1}")
+
+else:
+    print("Essa equação não possui raízes")
