cf3855ee1d4557ebd9ffad9e49d5c6c79f2037df
Biancorine <Lybra396@gmail.com>
2026-09-17 23:08:20 -0300

    asd

--- Problem 04/Assignment01.py
+++ Problem 04/Assignment01.py
@@ -9,3 +9,5 @@ while lados < 3 or lados > 5:
     elif lados > 5:
         print("Poligono não identificado, insira outro valor")
         lados = int(input("Quantos lados tem o poligono?: "))
+
+print("bogos binted")
