a86b16c1235aae66d9329b232ea4aec3b7ee61aa
Biancorine <Lybra396@gmail.com>
2026-09-18 00:09:11 -0300

    AAAAAA

--- Problem 04/Assignment01.py
+++ Problem 04/Assignment01.py
@@ -20,7 +20,7 @@ if lados == 3:
 
     p = (lado1 + lado2 + lado3) / 2
     area = math.sqrt(p * (p - lado1) * (p - lado2) * (p - lado3))
-    printf("A area do triangulo é: {area}cm²")
+    print(f"A area do triangulo é: {area}cm²")
     sys.exit()
     # Esse código calcula a área de um triângulo usando a fórmula de Heron, deve funcionar com qualquer triângulo.
 
@@ -29,7 +29,7 @@ if lados == 4:
     print("O poligono é um quadrado")
     lado = float(input("Digite o valor do lado cm: "))
     area = lado * lado
-    printf("A area do quadrado é: {area}cm²")
+    print(f"A area do quadrado é: {area}cm²")
     sys.exit()
     # Esse código calcula a área de um quadrado, que é simplesmente o lado ao quadrado.
 
@@ -38,6 +38,6 @@ if lados == 5:
     print("O poligono é um pentagono")
     lado = float(input("Digite o valor do lado cm: "))
     area = (5 * lado * lado) / (4 * math.tan(math.pi / 5))
-    printf("A area do pentagono é: {area}cm²")
+    print(f"A area do pentagono é: {area}cm²")
     sys.exit()
     # Esse código calcula a área de um pentágono regular usando a fórmula: (5 * lado^2) / (4 * tan(π/5)), funciona apenas com pentágonos regulares.
